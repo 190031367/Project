b5521aab1cf320273138e741b896962afc36ca70
priya <ilavishnu2002@gmail.com>
2021-11-10 10:47:59 +0530

    new Commit final

---
+++ src/main/java/com/example/demo/Cdd2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cdd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cdd2Application.class, args);
+	}
+
+}
